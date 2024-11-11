110c838510ddc244eea76890ba1652fabf5d9ec3
foryouamira <siteweb894@gmail.com>
2024-11-11 02:08:13 -0500

    added ch2

--- src/App.js
+++ src/App.js
@@ -2,28 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import Player from "@madzadev/audio-player";
 import "@madzadev/audio-player/dist/index.css";
-import sound from './content/CH1ST2.mp3'
+import CH1ST2 from './content/CH1ST2.mp3'
+import CH2ST2 from './content/CH2ST2.mp3'
 const tracks = [
   {
-    url: sound,
+    url: CH1ST2,
     title: "Chapter 1. The Sign of Four",
     tags: ["house"],
   },
   {
-    url: "https://audioplayer.madza.dev/Madza-Late_Night_Drive.mp3",
-    title: "Madza - Late Night Drive",
-    tags: ["dnb"],
-  },
-  {
-    url: "https://audioplayer.madza.dev/Madza-Persistence.mp3",
-    title: "Madza - Persistence",
-    tags: ["dubstep"],
+    url: CH2ST2,
+    title: "Chapter 2. The Statement of the Case",
+    tags: ["house"],
   },
 ];
 
 function App() {
   return (
     <div className="App">
+      <title>My voice</title>
+      <title>Sherlock</title>
       <header className="App-header">
         <Player trackList={tracks} />
       </header>
