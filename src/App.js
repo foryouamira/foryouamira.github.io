19be0005f68bef549367603c6a93d7019a361c61
foryouamira <siteweb894@gmail.com>
2024-11-16 13:49:30 -0500

    almost done

--- src/App.js
+++ src/App.js
@@ -4,26 +4,57 @@ import Player from "@madzadev/audio-player";
 import "@madzadev/audio-player/dist/index.css";
 import CH1ST2 from './content/CH1ST2.mp3'
 import CH2ST2 from './content/CH2ST2.mp3'
+import RDP1 from './content/LeagueofRedHeadsPT1.mp3'
+import Ima from './content/Image.mp3'
+import confess from './content/confession.mp3'
+import RDP2 from './content/redheadp2.mp3'
 const tracks = [
   {
     url: CH1ST2,
     title: "Chapter 1. The Sign of Four",
-    tags: ["house"],
+    tags: ["Spooky Murder"],
   },
   {
     url: CH2ST2,
     title: "Chapter 2. The Statement of the Case",
-    tags: ["house"],
+    tags: ["Spooky Mormons"],
+  },
+];
+const track2 = [
+  {
+    url: RDP1,
+    title: "Part 1. Mystery",
+    tags: ["Red Heads"],
+  },
+  {
+    url: RDP2,
+    title: "Part 2. Intrigue",
+    tags: ["Red Heads"],
   },
 ];
 
+const track3 = [
+  {
+    url: Ima,
+    title: "Through My Eyes",
+    tags: ["love"],
+  },
+  {
+    url: confess,
+    title: "A confession",
+    tags: ["love"],
+  },
+];
 function App() {
   return (
     <div className="App">
-      <title>My voice</title>
-      <title>Sherlock</title>
       <header className="App-header">
+        <h1>ST2</h1>
         <Player trackList={tracks} />
+        <h1>Red Head</h1>
+        <Player trackList={track2}/>
+        <h1>Words to a sleeping princess</h1>
+        <Player trackList={track3}/>
       </header>
     </div>
   );
